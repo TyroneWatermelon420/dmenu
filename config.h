1ec5597774f057f205c6eaadcfc9c61b0db5ac95
Stormx920 <eriklrosebrock@tutanota.com>
2023-07-15 23:12:27 +0200

    FIX v2

---
+++ config.h
@@ -0,0 +1,28 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+
+static const int user_bt = 10;               /* add an defined amount of pixels to the bar height */
+static const int user_bh = 10;
+
+static const char *fonts[] = {
+	"Hack Nerd Font Mono:size=10"
+};
+
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#bbbbbb", "#2e3440" },
+	[SchemeSel] = { "#eeeeee", "#5e81ac" },
+	[SchemeOut] = { "#000000", "#00ffff" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
