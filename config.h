ea2a2c0a3ba194d7eb0bfe44286f2a80ecf9c1b0
Stormx920 <eriklrosebrock@tutanota.com>
2023-07-16 09:16:11 +0200

    FIX FIX FIX

--- config.h
+++ config.h
@@ -2,20 +2,19 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-
-static const int user_bt = 10;               /* add an defined amount of pixels to the bar height */
+static const int user_bt = 0;               /* add an defined amount of pixels to the bar height */
 static const int user_bh = 10;
 
 static const char *fonts[] = {
-	"Hack Nerd Font Mono:size=10"
+	"monospace:size=10"
 };
-
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#2e3440" },
-	[SchemeSel] = { "#eeeeee", "#5e81ac" },
+	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeSel] = { "#eeeeee", "#005577" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
