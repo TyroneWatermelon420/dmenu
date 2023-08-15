e2f158fc5ebecad878ea45640b4bfb53ee7ac8dc
Stormx920 <eriklrosebrock@tutanota.com>
2023-08-15 19:00:52 +0200

    update

--- config.h
+++ config.h
@@ -5,10 +5,10 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const int user_bt = 0;               /* add an defined amount of pixels to the bar height */
-static const int user_bh = 10;
+static const int user_bh = 12;
 
 static const char *fonts[] = {
-	"monospace:size=10"
+	"Hack Nerd Font Mono:size=11"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
