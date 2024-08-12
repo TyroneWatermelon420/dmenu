f0fdbf8e48b79d7d7828c7c51830ea755f61443e
TyroneWatermelon420 <erik.rosebrock@tutanota.com>
2024-08-12 19:15:02 +0200

    Apply Catppuccin Mocha theme

--- config.h
+++ config.h
@@ -13,11 +13,10 @@ static const char *fonts[] = {
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-    /*     fg         bg       */
-    [SchemeNorm] = { "#cccccc", "#282c34" },
-	[SchemeSel] = { "#1c1f24", "#c678dd" },
-	[SchemeOut] = { "#000000", "#51afef" },
-	[SchemeMid] = { "#d7d7d7", "#1c1f24" },
+	/*     fg         bg       */
+	[SchemeNorm] = { "#cdd6f4", "#1e1e2e" },
+	[SchemeSel] = { "#1e1e2e", "#89dceb" },
+	[SchemeOut] = { "#000000", "#89dceb" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
